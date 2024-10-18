de1c6e968a30c029ff18110dd98913f9382b8bb0
KeanaClarisse <143857078+KeanaClarisse@users.noreply.github.com>
2024-10-18 19:11:17 +0800

    Update ClientModel.cs

--- Model/ClientModel.cs
+++ Model/ClientModel.cs
@@ -1,7 +1,14 @@
-﻿namespace Model
+namespace Model
 {
     public class ClientModel
     {
+        public class Client
+        {
+            public int Id { get; set; }
+            public string FirstName { get; set; }
+            public string LastName { get; set; }
+            public string Email { get; set; }
+        }
 
     }
 }
